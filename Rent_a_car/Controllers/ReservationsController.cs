ee7f7fc15e9aed296c28eaac7e58554b5e50b15f
anna-2323 <79278381+anna-2323@users.noreply.github.com>
2022-03-27 13:05:11 +0300

    Add files via upload

--- Rent_a_car/Controllers/ReservationsController.cs
+++ Rent_a_car/Controllers/ReservationsController.cs
@@ -15,25 +15,46 @@ namespace Rent_a_car.Controllers
     public class ReservationsController : Controller
     {
         private Rent_a_carDBContext _database;
+        List<Reservations> reservationHistory;
         public ReservationsController(Rent_a_carDBContext database)
         {
             _database = database;
         }
-        public ActionResult Index()
+        //Method is async to prevent crashes if requests are being sent too fast
+        #nullable enable
+        public async Task<ViewResult> Index(string? Id)
         {
+            //Shows all reservations by default (when id is -1), otherwise shows only reservations for the chosen car id
+            int carId = Id is null ? -1 : int.Parse(Id);
+
             var user = HttpContext.Session.GetObject<Users>("loggedUser");
-            List<Reservations> reservationHistory = new List<Reservations>();
+            reservationHistory = new List<Reservations>();
+            //Converts the cars to dropdown list items
+            IEnumerable<SelectListItem> allCars = _database.Cars.Select(car => new SelectListItem
+            {
+                Value = car.Id.ToString(),
+                Text = car.Model
+            });
+
             if (user.IsAdmin == 0)
                 reservationHistory = _database.Reservations.Include(r => r.Car).Where(r => r.UserId == user.Id).ToList();
-            else
+            else if(carId == -1)
                 reservationHistory = _database.Reservations.Include(r => r.Car).ToList();
+            else
+                reservationHistory = _database.Reservations.Include(r => r.Car).Where(r => r.CarId == carId).ToList();
+
             ViewData["reservationHistory"] = reservationHistory;
-            _database.Reservations.Where(r => r.Status == (int)Statuses.Approved && r.DateOfReservation > DateTime.Now).ForEachAsync(r => r.Status = (int)Statuses.Ongoing);
-            _database.Reservations.Where(r => r.Status == (int)Statuses.Ongoing && r.EndDate < DateTime.Now).ForEachAsync(r => r.Status = (int)Statuses.Completed);
-            _database.Reservations.Where(r => r.EndDate < DateTime.Today).ForEachAsync(r => r.Status = (int)Statuses.Missed);
-            _database.SaveChanges();
+            ViewData["allCars"] = allCars;
+
+            //Adjusts the reservations' statuses
+            await _database.Reservations.Where(r => r.Status == (int)Statuses.Approved && r.DateOfReservation > DateTime.Now).ForEachAsync(r => r.Status = (int)Statuses.Ongoing);
+            await _database.Reservations.Where(r => r.Status == (int)Statuses.Ongoing && r.EndDate < DateTime.Now).ForEachAsync(r => r.Status = (int)Statuses.Completed);
+            await _database.Reservations.Where(r => r.EndDate < DateTime.Today).ForEachAsync(r => r.Status = (int)Statuses.Missed);
+
+            await _database.SaveChangesAsync();
             return View();
         }
+        #nullable disable
 
         [HttpPost]
         public ActionResult ApproveReservation(int id)
@@ -72,17 +93,17 @@ namespace Rent_a_car.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult MakeReservations(ReservationsVM input)
+        public Task<ViewResult> MakeReservations(ReservationsVM input)
         {
             if (!this.ModelState.IsValid)
-                return View(input);
+                return Task.FromResult(View(input));
             var user = HttpContext.Session.GetObject<Users>("loggedUser");
             var reservation = input.GetReservation();
             reservation.Status = (int)Statuses.Upcoming;
             reservation.UserId = user.Id;
             _database.Reservations.Add(reservation);
             _database.SaveChanges();
-            return Index();
+            return Index(null);
         }
     }
 }
